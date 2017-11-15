b6d31279dcbc516b3629228982b803cd382b7d99
Kenneth Ascheri <ascherik@lanecc.edu>
2017-11-14 20:46:52 -0800

    Define Create_Task class, remove old deployment notes

--- create_task.py
+++ create_task.py
@@ -5,20 +5,64 @@ from subprocess import Popen, PIPE
 import os
 import getpass as gp
 import sys
-# import config
 
-timeout = 180 * 60.0 # xx minutes @ Sixty seconds
+
+class Create_Task:
+  timeout = 120 * 60.0 # xx minutes @ Sixty seconds
+  res_golem_header = "/Users/ascherik/Downloads/golem-header.blend"
+  res_airplane = "/Users/ascherik/Downloads/Golem\ Airplane"
+  filename = "tmp.task"
+  
+  def __init__(self):
+    pass
+
+  def add_new_task(self):
+    command_str = " ".join("~/Downloads/golem-0.9.0/golemcli","tasks","create",self.build_golem_header_task(2))
+    proc = Popen(command_str, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+  def build_golem_header_task(self,level):
+    ght = None
+    if level == 1:
+      ght = self.build_simple_golem_header_task(10,300,200)
+    elif level == 2:
+      ght = self.build_simple_golem_header_task(100,3000,2000)
+    return ght
+
+  def build_simple_golem_header_task(self,subtasks,height,width):
+    ts = {
+      "name":"Golem Task Simple",
+      "type": "Blender",
+      "subtasks": subtasks,
+      "options": {
+          "frame_count": 1,
+          "output_path": "",
+          "format": "PNG",
+          "resolution": [
+              height,
+              width
+          ],
+          "frames": "1",
+          "compositing": False
+      },
+      "timeout": "20:00:00",
+      "subtask_timeout": "0:35:00",
+      "bid": 5.0,
+      "resources": [
+          self.res_golem_header
+      ]
+    }
+    with open(self.filename,'w') as f:
+      f.write(str(ts))
+    return self.filename
+
 
 def __main__(argv):
 
-    l = task.LoopingCall(add_new_task)
-    l.start(timeout) # call every sixty seconds
+  ct = Create_Task()
+  l = task.LoopingCall(ct.add_new_task)
+  l.start(timeout) # call every sixty seconds
 
-    reactor.run()
+  reactor.run()
 
-def add_new_task():  
-  proc = Popen('~/golem-0.9.0/golemcli tasks create sample_tasks/task.task', shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-  
-  
 if __name__ == '__main__':
-    __main__(sys.argv[1:])
+  __main__(sys.argv[1:])
