a90fd2633c39cf61b468d2ec2655dbb65eed9b21
Kenny Ascheri <kascheri12@gmail.com>
2018-04-26 20:43:20 -0700

    Move deprecated code to bottom of file and comment.

--- analyze_logs.py
+++ analyze_logs.py
@@ -220,26 +220,6 @@ class Analyze_Logs:
     name = str(node[self._nn_index])+"("+node[self._id_index][:10]+")"
     return name
 
-  def build_y_axis_dict(self,d,x_axis):
-    SUCCESS_THRESHOLD = 5
-    nodes = d
-    y_axis_dict = {}
-
-    for timestamp in x_axis:
-      # for each record in the list of data points cooresponding to the timestamp
-      # and the node's total success are greater than the threshold
-      for record in [x for x in nodes['data'] if x[self._ts_index] == timestamp and float(x[self._ss_index]) >= SUCCESS_THRESHOLD]:
-        # Define the key as the node's name plus first 10 characters of the node_id
-        key = self.get_node_name(nodes['header'],record)
-        # Check if this dict key exists in the dict of nodes to be plotted
-        # Init dict value as list
-        if y_axis_dict.get(key) is None:
-          y_axis_dict[key] = []
-        # Append the specific x-y coordinate with the formatted
-        # timestamp and the number of successful subtasks
-        y_axis_dict[key].append([self.get_formatted_time(timestamp),record[self._ss_index] or None])
-    return y_axis_dict
-
   def build_y_axis_dict_for_network_summary(self,d,x_axis):
     y_axis_dict = {
         'Node Count':[],
@@ -279,84 +259,14 @@ class Analyze_Logs:
       y_axis_dict[key_names[3]].append([formatted_ts,summary_cpu_cores])
     return y_axis_dict
 
-  def build_y_axis_dict_for_change_in_subtasks(self,x_axis):
-    nodes = self.d
-    y_axis_dict = {
-        'Node Count':[],
-        'Subtask Computation Change between logs':[],
-        'Subtask Computation Change in 1 hour':[],
-        'Subtask Computation Change in 24 hours':[],
-        'Subtask Computation Change in 7 days':[]
-    }
-    key_names = [x for x in y_axis_dict.keys()]
-    subtotal_1_hr, subtotal_24_hr, subtotal_7_day = 0, 0, 0
-    i_1_hr, i_24_hr, i_7_day = 0, 0, 0
-
-    for i in range(len(x_axis)):
-      timestamp = x_axis[i]
-      cn = [x for x in nodes['data'] if x[self._ts_index] == timestamp]
-      formatted_ts = self.get_formatted_time(timestamp)
-
-      node_count = len(cn)
-      y_axis_dict[key_names[0]].append([formatted_ts,node_count])
-      subtotal_current = sum([float(x[self._ss_index]) for x in cn])
-
-      if i == 0:
-        y_axis_dict[key_names[2]].append([formatted_ts,subtotal_current])
-        y_axis_dict[key_names[3]].append([formatted_ts,subtotal_current])
-        y_axis_dict[key_names[4]].append([formatted_ts,subtotal_current])
-
-      if i > 0:
-        cnp = [x for x in nodes['data'] if x[self._ts_index] == x_axis[i-1]]
-        subtotal_prev = sum([float(x[self._ss_index]) for x in cnp])
-        y_axis_dict[key_names[1]].append([formatted_ts,subtotal_current-subtotal_prev])
-
-
-      #if the time between timestamp and x_axis[i_1_hr]
-      # is greater than 1hr then do the computation and
-      # mark x,y coordinates
-      if i>0 and (timestamp - x_axis[i_1_hr]) >= 3600:
-        # calculate sum of subtasks completed in network from all timestamps
-        # between i_1_hr and the current timestamp
-        sub_cur = subtotal_1_hr + sum([float(x[self._ss_index]) for x in cn])
-
-        # subtract the value just calculated above for the subtasks completed
-        # from the previously appended value in y_axis_dict
-        y_axis_dict[key_names[2]].append([formatted_ts,sub_cur - y_axis_dict[key_names[2]][-1][1]])
-
-        # set the new index for i_1_hr
-        i_1_hr = i
-        subtotal_1_hr = 0
-      else:
-        subtotal_1_hr += subtotal_current
-
-
-      if i>0 and (timestamp - x_axis[i_24_hr]) >= 86400:
-        sub_cur = subtotal_24_hr + sum([float(x[self._ss_index]) for x in cn])
-        y_axis_dict[key_names[3]].append([formatted_ts, sub_cur - y_axis_dict[key_names[3]][-1][1]])
-        i_24_hr = i
-        subtotal_24_hr = 0
-      else:
-        subtotal_24_hr += subtotal_current
-
-      if i>0 and (timestamp - x_axis[i_7_day]) >= 604800:
-        sub_cur = subtotal_7_day + sum([float(x[self._ss_index]) for x in cn])
-        y_axis_dict[key_names[4]].append([formatted_ts, sub_cur - y_axis_dict[key_names[4]][-1][1]])
-        i_7_day = i
-        subtotal_7_day = 0
-      else:
-        subtotal_7_day += subtotal_current
-
-    return y_axis_dict
-
   def build_y_axis_dict_for_summary_over_last_days(self,x_axis):
     y_axis_dict = {
       'Unique Node Count':[]
     }
-    
+
     key_names = [x for x in y_axis_dict.keys()]
     unique_nodes = []
-    
+
     for timestamp in x_axis:
       fts = self.get_formatted_time(timestamp)
       distinct_ids_before_ts = list(set([x[self._id_index] for x in self.d['data'] if x[self._ts_index] < timestamp]))
@@ -365,42 +275,12 @@ class Analyze_Logs:
       if cnt_distinct_ts_for_new_nodes:
         avg_new_for_ts = len(new_nodes_this_ts) / cnt_distinct_ts_for_new_nodes
         y_axis_dict[key_names[0]].append([fts,avg_new_for_ts])
-      
+
     return y_axis_dict
 
   def build_x_axis(self,d,log_cutoff_date=dt(2017,1,1)):
     x_axis = sorted(list(set([x[self._ts_index] for x in d['data'] if dt.fromtimestamp(x[self._ts_index]) > log_cutoff_date])))
     return x_axis
-
-  def print_change_in_subtask_success_graph(self,d,days_since_cutoff):
-    filename = 'golem-network-change-in-subtask-success.html'
-    log_cutoff_date = dt.today() - timedelta(days=days_since_cutoff)
-
-    x_axis = self.build_x_axis(d,log_cutoff_date)
-    y_axis_dict = self.build_y_axis_dict_for_change_in_subtasks(x_axis)
-    traces = []
-    lt = time.localtime()
-    pt = "%s%s%s-%s:%s%s" % (lt.tm_year,lt.tm_mon,lt.tm_mday,lt.tm_hour,lt.tm_min,time.tzname[0])
-
-    for key in y_axis_dict.keys():
-      traces.append(go.Scatter(
-      x = [x[0] for x in y_axis_dict[key]],
-      y = [x[1] for x in y_axis_dict[key]],
-      mode='lines',
-      connectgaps=False,
-      name=key
-      ))
-    layout = dict(title = 'Golem Network Change in Subtask Success',
-                xaxis = dict(title = 'Time'),
-                yaxis = dict(title = 'Change in Successful Subtask Computations on Golem Network')
-                )
-
-    data = traces
-    fig = dict(data=data,layout=layout)
-
-    plotly.offline.plot(fig, filename=filename, auto_open=False)
-    self.inject_google_analytics(filename)
-    return filename
 
   def print_summary_over_last_days_graph(self,days_since_cutoff):
     filename = 'summary_last_'+str(days_since_cutoff)+'_days.html'
@@ -418,7 +298,7 @@ class Analyze_Logs:
       y = [x[1] for x in y_axis_dict[key]],
       name=key
       ))
-    
+
     layout = go.Layout(
         title='Golem Network Summary over Last '+str(days_since_cutoff)+' days',
         xaxis=dict(
@@ -455,7 +335,7 @@ class Analyze_Logs:
     plotly.offline.plot(fig, filename=filename, auto_open=False)
     self.inject_google_analytics(filename)
     return filename
-    
+
   def print_daily_aggregate_totals(self,num_days_included):
     filename = 'daily_aggregate_totals_'+str(num_days_included)+'_days.html'
     log_cutoff_date = dt.today() - timedelta(days=num_days_included)
@@ -471,7 +351,7 @@ class Analyze_Logs:
       y = [x[1] for x in y_axis_dict[key]],
       name=key
       ))
-    
+
     layout = go.Layout(
         title='Golem Network Daily Aggregate Totals Last '+str(num_days_included)+' days',
         xaxis=dict(
@@ -548,39 +428,6 @@ class Analyze_Logs:
     self.inject_google_analytics(filename)
     return filename
 
-  def print_node_success_over_time_graph(self,d,days_since_cutoff):
-    filename = 'golem-network-success-report.html'
-    log_cutoff_date = dt.today() - timedelta(days=days_since_cutoff)
-
-    x_axis = self.build_x_axis(d,log_cutoff_date)
-    y_axis_dict = self.build_y_axis_dict(d,x_axis)
-    traces = []
-    lt = time.localtime()
-    pt = time.strftime("%Y%m%d-%H:%M%Z",lt)
-
-    # For each node to be plotted
-    for key in sorted(y_axis_dict.keys()):
-      # Append a scatter plot for the specific node based on time and subtasks successes
-      traces.append(go.Scatter(
-      x = [x[0] for x in y_axis_dict[key]],
-      y = [x[1] for x in y_axis_dict[key]],
-      mode='lines',
-      connectgaps=None,
-      name=key
-      ))
-
-    layout = dict(title = 'Golem Network Successful Subtask Computations by Node as of ' + pt,
-              xaxis = dict(title = 'Time'),
-              yaxis = dict(title = 'Successful Subtask Computations on Golem Network')
-              )
-
-    data = traces
-    fig = dict(data=data,layout=layout)
-
-    plotly.offline.plot(fig, filename=filename, auto_open=False)
-    self.inject_google_analytics(filename)
-    return filename
-
   def print_daily_avg_nodes_connected(self,num_days_included):
     filename = 'daily_avg_nodes_connected_'+str(num_days_included)+'_days.html'
     log_cutoff_date = dt.today() - timedelta(days=num_days_included)
@@ -592,13 +439,13 @@ class Analyze_Logs:
     list_of_dates = sorted(self.get_list_of_dates_for_data())
     for d in list_of_dates:
       plot_pairs.append([d,self.get_avg_nodes_connected_on_date(d)])
-    
+
     traces.append(go.Bar(
     x = [x[0] for x in plot_pairs],
     y = [x[1] for x in plot_pairs],
     name='Avg Nodes Connected'
     ))
-    
+
     layout = go.Layout(
         title='Golem Network Daily Average Nodes Connected Last '+str(num_days_included)+' Days',
         xaxis=dict(
@@ -635,7 +482,7 @@ class Analyze_Logs:
     plotly.offline.plot(fig, filename=filename, auto_open=False)
     self.inject_google_analytics(filename)
     return filename
-    
+
   def inject_google_analytics(self,filename):
     analytics_string = '''
         <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -679,7 +526,7 @@ class Analyze_Logs:
     all_nodes_logged_on_date = [x for x in self.d['data'] if dt.date(dt.fromtimestamp(x[self._ts_index])) == td]
     all_timestamps_logged_on_date = set([x[self._ts_index] for x in all_nodes_logged_on_date])
     return len(all_nodes_logged_on_date) / len(all_timestamps_logged_on_date)
-    
+
   def get_avg_new_unique_node_count_on_date(self, td):
     distinct_node_ids_logged_on_date = list(set([x[self._id_index] for x in self.d['data'] if dt.date(dt.fromtimestamp(x[self._ts_index])) == td]))
     distinct_timestamps_on_date = list(set([x[self._ts_index] for x in [x for x in self.d['data'] if dt.date(dt.fromtimestamp(x[self._ts_index])) == td]]))
@@ -702,20 +549,20 @@ class Analyze_Logs:
       total_count_subtasks_success = sum([x[self._ss_index] for x in list_nodes_on_date])
       return total_count_subtasks_success / len(list_nodes_on_date)
     return 0
-  
+
   def get_daily_aggregate_totals(self):
     dailytot = {
       'New Unique':[],
       'Subtasks Requested':[],
       'Subtasks Completed':[]
     }
-    
+
     list_of_dates = sorted(self.get_list_of_dates_for_data())
     for d in list_of_dates:
       dailytot['New Unique'].append([d,self.get_avg_new_unique_node_count_on_date(d)])
       dailytot['Subtasks Requested'].append([d,self.get_avg_requested_subtasks_on_date(d)])
       dailytot['Subtasks Completed'].append([d,self.get_avg_subtasks_success_on_date(d)])
-      
+
     return dailytot
 
   def load_data(self):
@@ -848,3 +695,157 @@ class Analyze_Logs:
           f2.write(''.join(r[:121])+"\n"+''.join(r[122:]))
       remove('node_logs/'+filename)
       move('node_logs/'+filename+'.tmp', 'node_logs/'+filename)
+
+  # Deprecated graphs
+  # def print_change_in_subtask_success_graph(self,d,days_since_cutoff):
+  #   filename = 'golem-network-change-in-subtask-success.html'
+  #   log_cutoff_date = dt.today() - timedelta(days=days_since_cutoff)
+  #
+  #   x_axis = self.build_x_axis(d,log_cutoff_date)
+  #   y_axis_dict = self.build_y_axis_dict_for_change_in_subtasks(x_axis)
+  #   traces = []
+  #   lt = time.localtime()
+  #   pt = "%s%s%s-%s:%s%s" % (lt.tm_year,lt.tm_mon,lt.tm_mday,lt.tm_hour,lt.tm_min,time.tzname[0])
+  #
+  #   for key in y_axis_dict.keys():
+  #     traces.append(go.Scatter(
+  #     x = [x[0] for x in y_axis_dict[key]],
+  #     y = [x[1] for x in y_axis_dict[key]],
+  #     mode='lines',
+  #     connectgaps=False,
+  #     name=key
+  #     ))
+  #   layout = dict(title = 'Golem Network Change in Subtask Success',
+  #               xaxis = dict(title = 'Time'),
+  #               yaxis = dict(title = 'Change in Successful Subtask Computations on Golem Network')
+  #               )
+  #
+  #   data = traces
+  #   fig = dict(data=data,layout=layout)
+  #
+  #   plotly.offline.plot(fig, filename=filename, auto_open=False)
+  #   self.inject_google_analytics(filename)
+  #   return filename
+  #
+  # def build_y_axis_dict_for_change_in_subtasks(self,x_axis):
+  #   nodes = self.d
+  #   y_axis_dict = {
+  #       'Node Count':[],
+  #       'Subtask Computation Change between logs':[],
+  #       'Subtask Computation Change in 1 hour':[],
+  #       'Subtask Computation Change in 24 hours':[],
+  #       'Subtask Computation Change in 7 days':[]
+  #   }
+  #   key_names = [x for x in y_axis_dict.keys()]
+  #   subtotal_1_hr, subtotal_24_hr, subtotal_7_day = 0, 0, 0
+  #   i_1_hr, i_24_hr, i_7_day = 0, 0, 0
+  #
+  #   for i in range(len(x_axis)):
+  #     timestamp = x_axis[i]
+  #     cn = [x for x in nodes['data'] if x[self._ts_index] == timestamp]
+  #     formatted_ts = self.get_formatted_time(timestamp)
+  #
+  #     node_count = len(cn)
+  #     y_axis_dict[key_names[0]].append([formatted_ts,node_count])
+  #     subtotal_current = sum([float(x[self._ss_index]) for x in cn])
+  #
+  #     if i == 0:
+  #       y_axis_dict[key_names[2]].append([formatted_ts,subtotal_current])
+  #       y_axis_dict[key_names[3]].append([formatted_ts,subtotal_current])
+  #       y_axis_dict[key_names[4]].append([formatted_ts,subtotal_current])
+  #
+  #     if i > 0:
+  #       cnp = [x for x in nodes['data'] if x[self._ts_index] == x_axis[i-1]]
+  #       subtotal_prev = sum([float(x[self._ss_index]) for x in cnp])
+  #       y_axis_dict[key_names[1]].append([formatted_ts,subtotal_current-subtotal_prev])
+  #
+  #
+  #     #if the time between timestamp and x_axis[i_1_hr]
+  #     # is greater than 1hr then do the computation and
+  #     # mark x,y coordinates
+  #     if i>0 and (timestamp - x_axis[i_1_hr]) >= 3600:
+  #       # calculate sum of subtasks completed in network from all timestamps
+  #       # between i_1_hr and the current timestamp
+  #       sub_cur = subtotal_1_hr + sum([float(x[self._ss_index]) for x in cn])
+  #
+  #       # subtract the value just calculated above for the subtasks completed
+  #       # from the previously appended value in y_axis_dict
+  #       y_axis_dict[key_names[2]].append([formatted_ts,sub_cur - y_axis_dict[key_names[2]][-1][1]])
+  #
+  #       # set the new index for i_1_hr
+  #       i_1_hr = i
+  #       subtotal_1_hr = 0
+  #     else:
+  #       subtotal_1_hr += subtotal_current
+  #
+  #
+  #     if i>0 and (timestamp - x_axis[i_24_hr]) >= 86400:
+  #       sub_cur = subtotal_24_hr + sum([float(x[self._ss_index]) for x in cn])
+  #       y_axis_dict[key_names[3]].append([formatted_ts, sub_cur - y_axis_dict[key_names[3]][-1][1]])
+  #       i_24_hr = i
+  #       subtotal_24_hr = 0
+  #     else:
+  #       subtotal_24_hr += subtotal_current
+  #
+  #     if i>0 and (timestamp - x_axis[i_7_day]) >= 604800:
+  #       sub_cur = subtotal_7_day + sum([float(x[self._ss_index]) for x in cn])
+  #       y_axis_dict[key_names[4]].append([formatted_ts, sub_cur - y_axis_dict[key_names[4]][-1][1]])
+  #       i_7_day = i
+  #       subtotal_7_day = 0
+  #     else:
+  #       subtotal_7_day += subtotal_current
+  #
+  #   return y_axis_dict
+  #
+  # def print_node_success_over_time_graph(self,d,days_since_cutoff):
+  #   filename = 'golem-network-success-report.html'
+  #   log_cutoff_date = dt.today() - timedelta(days=days_since_cutoff)
+  #
+  #   x_axis = self.build_x_axis(d,log_cutoff_date)
+  #   y_axis_dict = self.build_y_axis_dict(d,x_axis)
+  #   traces = []
+  #   lt = time.localtime()
+  #   pt = time.strftime("%Y%m%d-%H:%M%Z",lt)
+  #
+  #   # For each node to be plotted
+  #   for key in sorted(y_axis_dict.keys()):
+  #     # Append a scatter plot for the specific node based on time and subtasks successes
+  #     traces.append(go.Scatter(
+  #     x = [x[0] for x in y_axis_dict[key]],
+  #     y = [x[1] for x in y_axis_dict[key]],
+  #     mode='lines',
+  #     connectgaps=None,
+  #     name=key
+  #     ))
+  #
+  #   layout = dict(title = 'Golem Network Successful Subtask Computations by Node as of ' + pt,
+  #             xaxis = dict(title = 'Time'),
+  #             yaxis = dict(title = 'Successful Subtask Computations on Golem Network')
+  #             )
+  #
+  #   data = traces
+  #   fig = dict(data=data,layout=layout)
+  #
+  #   plotly.offline.plot(fig, filename=filename, auto_open=False)
+  #   self.inject_google_analytics(filename)
+  #   return filename
+  #
+  # def build_y_axis_dict(self,d,x_axis):
+  #   SUCCESS_THRESHOLD = 5
+  #   nodes = d
+  #   y_axis_dict = {}
+  #
+  #   for timestamp in x_axis:
+  #     # for each record in the list of data points cooresponding to the timestamp
+  #     # and the node's total success are greater than the threshold
+  #     for record in [x for x in nodes['data'] if x[self._ts_index] == timestamp and float(x[self._ss_index]) >= SUCCESS_THRESHOLD]:
+  #       # Define the key as the node's name plus first 10 characters of the node_id
+  #       key = self.get_node_name(nodes['header'],record)
+  #       # Check if this dict key exists in the dict of nodes to be plotted
+  #       # Init dict value as list
+  #       if y_axis_dict.get(key) is None:
+  #         y_axis_dict[key] = []
+  #       # Append the specific x-y coordinate with the formatted
+  #       # timestamp and the number of successful subtasks
+  #       y_axis_dict[key].append([self.get_formatted_time(timestamp),record[self._ss_index] or None])
+  #   return y_axis_dict
