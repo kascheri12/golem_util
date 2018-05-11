9f13862bf1d47573e112b2de81bcab5f25c0eb49
Kenneth Ascheri <ascherik@lanecc.edu>
2018-05-10 23:06:09 -0700

    Refactor moving graphs after each one is built and commit it right away.

--- golem_graphing.py
+++ golem_graphing.py
@@ -20,6 +20,29 @@ class Golem_Graphing():
   def get_pretty_time(self):
     lt = time.localtime()
     return time.strftime("%Y%m%d-%H:%M %Z",lt)
+
+  def move_and_commit_graph(self,v_filepath):
+    try:
+      copy(config.build_graphs_dir+fn,config.kascheri12_github_io_graphs_dir+fn)
+    except:
+      print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error printing and/or moving graph<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+      traceback.print_exc(file=sys.stdout)
+      print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error printing and/or moving graph<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+      
+    try:
+      od = os.getcwd()
+      os.chdir(config.kascheri12_github_io_dir)
+      os.system('git pull')
+      os.system('git checkout master')
+      for fn in filenames:
+        os.system('git add ' + fn)
+      os.system('git commit -m "automated commit for daily-golem-graphs"')
+      os.system('git push')
+      os.chdir(od)
+    except:
+      print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error during git process<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+      traceback.print_exc(file=sys.stdout)
+      print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error during git process<<<<<<<<<<<<<<<<<<<<<<<<<<<")
     
   def daily_graph_refresh(self):
     print("Begin daily_graph_refresh: "+self.get_pretty_time())
@@ -27,31 +50,12 @@ class Golem_Graphing():
     a = al.Analyze_Data()
 
     try:
-      filenames.append(a.print_daily_aggregate_totals(30))
-      filenames.append(a.print_daily_avg_nodes_connected(30))
-      # filenames.append(a.print_new_unique_over_last_days_graph(10))
-      filenames.append(a.print_network_summary_over_time_graph(30))
-      try:
-        for fn in filenames:
-          copy(config.build_graphs_dir+fn,config.kascheri12_github_io_graphs_dir+fn)
-        try:
-          od = os.getcwd()
-          os.chdir(config.kascheri12_github_io_dir)
-          os.system('git pull')
-          os.system('git checkout master')
-          for fn in filenames:
-            os.system('git add ' + fn)
-          os.system('git commit -m "automated commit for daily-golem-graphs"')
-          os.system('git push')
-          os.chdir(od)
-        except:
-          print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error during git process<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-          traceback.print_exc(file=sys.stdout)
-          print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error during git process<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-      except:
-        print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error printing and/or moving graph<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-        traceback.print_exc(file=sys.stdout)
-        print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error printing and/or moving graph<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+      self.move_and_commit_graph(a.print_avg_daily_subtask_totals(90))
+      self.move_and_commit_graph(a.print_avg_daily_nodes_connected(90))
+      self.move_and_commit_graph(a.print_avg_daily_subtask_totals(90))
+      self.move_and_commit_graph(a.print_avg_daily_failed_totals(90))
+      # self.move_and_commit_graph(a.print_new_unique_over_last_days_graph(30))
+      # self.move_and_commit_graph(a.print_network_summary_over_time_graph(30))
     except:
       print(self.get_pretty_time() + " - >>>>>>>>>>>>>>>>>>>>>>>>>>>Error creating graph<<<<<<<<<<<<<<<<<<<<<<<<<<<")
       traceback.print_exc(file=sys.stdout)
