09b9b85688e0f7af2215d1e4242d49329474f84f
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-02 22:58:06 -0800

    Add entire function to dashboard markdown

--- analyze_data.py
+++ analyze_data.py
@@ -595,9 +595,42 @@ title: Dashboard
 
 
 <script type='javascript'>
-$(document).ready(function() {{
-init_gauge({guage_percent_change_subtasks_success_past_day_value});
-}});
+
+  $(document).ready(function() {{
+    var opts = {{
+    angle: 0, // The span of the gauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 1, // Relative radius
+    pointer: {{
+      length: 0.6, // // Relative to gauge radius
+      strokeWidth: 0.035, // The thickness
+      color: '#000000' // Fill color
+    }},
+    staticLabels: {{
+      font: "20px sans-serif",  // Specifies font
+      labels: [-50,-30,-10,10,30,50],  // Print labels at these values
+      color: "#000000",  // Optional: Label text color
+      fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    }},
+    limitMax: false,     // If false, max value increases automatically if value > maxValue
+    limitMin: false,     // If true, the min value of the gauge will be fixed
+    colorStart: '#CF0E00',   // Colors
+    colorStop: '#47DA37',    // just experiment with them
+    strokeColor: '#E0E0E0',  // to see which ones work best for you
+    generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    }};
+    var target = document.getElementById('guage_percent_change_subtasks_success_past_day'); // your canvas element
+    var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+    gauge.maxValue = 50; // set max gauge value
+    gauge.setMinValue(-50);  // Prefer setter over gauge.minValue = 0
+    gauge.animationSpeed = 32; // set animation speed (32 is default value)
+    gauge.set(pin_value); // set actual value
+    gauge.setTextField(document.getElementById('span_guage_percent_change_subtasks_success_past_day'),3);
+    init_gauge({guage_percent_change_subtasks_success_past_day_value});
+  }});
+  
 </script>
 
 <iframe style="width:100%;height:400px" src="https://kascheri12.github.io/graphs/meter_subtasks_success_change_past_day.html"></iframe>
